fc5538495164cfb5d98646a0f5cc74a5ccbde87b
srikiranpanchavati <srikiran.phoenix@gmail.com>
2016-08-19 14:07:39 -0700

    updated to remove duplicate vertex object creations.

--- Graphs/src/Graph.java
+++ Graphs/src/Graph.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 class Vertex {
 	public String data;
@@ -11,12 +11,12 @@ class Vertex {
 		this.headOfEdgeList = null;
 	}
 
-	public Edge addEdge(Vertex vertex) {
+	public void addEdge(Vertex vertex) {
 		Edge toAdd = new Edge(vertex);
 		if (this.headOfEdgeList != null) {
 			toAdd.next = this.headOfEdgeList;
 		}
-		return toAdd;
+		this.headOfEdgeList = toAdd;
 	}
 }
 
@@ -31,25 +31,41 @@ class Edge {
 }
 
 public class Graph {
-	public HashMap<String, Vertex> vertexMap;
+	public HashMap<String, Vertex> adjacencyList;
+	public List<List<Boolean>> adjacencyMatrix;
 
 	public Graph(String[] input) {
-		vertexMap = new HashMap<>();
+		adjacencyList = new HashMap<>();
 		for (int i = 0; i < input.length; i++) {
-			String[] split = input[i].split(":");
-			if (!vertexMap.containsKey(split[0])) {
-				vertexMap.put(split[0], new Vertex(split[0]));
-			}
-			Vertex vertex = vertexMap.get(split[0]);
-			vertex.headOfEdgeList = vertex.addEdge(new Vertex(split[1]));
+			createVertices(input[i]);			
 		}
+		for(int i = 0; i < input.length ; i++){
+			adjacencyListRepresentation(input[i]);
+		}
+	}
+	
+	private void createVertices(String input){
+		String[] split = input.split(":");
+		if(!adjacencyList.containsKey(split[0])){
+			adjacencyList.put(split[0], new Vertex(split[0]));
+		}
+		if(!adjacencyList.containsKey(split[1])){
+			adjacencyList.put(split[1], new Vertex(split[1]));
+		}	
+	}
+
+	private void adjacencyListRepresentation(String input) {		
+		String[] split = input.split(":");
+		Vertex source = adjacencyList.get(split[0]);
+		Vertex destination = adjacencyList.get(split[1]);
+		source.addEdge(destination);
 	}
 
 	public static void main(String ar[]) {
 		String[] input = { "a:b", "b:c", "a:c", "b:d", "c:d" };
 		Graph graph = new Graph(input);
-		for (Map.Entry<String, Vertex> entry : graph.vertexMap.entrySet()) {
-			System.out.print(entry.getKey() + "\t|\t");
+		for (Map.Entry<String, Vertex> entry : graph.adjacencyList.entrySet()) {
+			System.out.print(entry.getKey() + "\t| ");
 			Vertex vertex = entry.getValue();
 			Edge edge = vertex.headOfEdgeList;
 			while (edge != null) {
